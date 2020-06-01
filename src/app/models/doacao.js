5f3ed6d41acddec9785155dfcb3fa722842d7306
gsena <gsena190@gmail.com>
2020-06-01 04:30:14 -0300

    remove id

--- src/app/models/doacao.js
+++ src/app/models/doacao.js
@@ -3,7 +3,6 @@ import mongoose, { Schema } from 'mongoose';
 Schema = mongoose.Schema;
 
 const DoacaoSchema = new Schema({
-    id: String,
     date: String,
     local: String,
     qntBolsas: String
